43beec42bff18f906bc4b4beab56769e3bdcd285
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-04-29 09:52:25 +0100

    changement dans la branch master

--- src/main/java/com/sip/ams/DevopsGkApplication.java
+++ src/main/java/com/sip/ams/DevopsGkApplication.java
@@ -9,6 +9,7 @@ public class DevopsGkApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello GK");
 		System.out.println("Un premier code de amine");
+		System.out.println("Deuxième changement dans la branch master");
 		SpringApplication.run(DevopsGkApplication.class, args);
 	}
 
